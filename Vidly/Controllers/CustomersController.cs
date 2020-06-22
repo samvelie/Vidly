fca83da2d0b1b76e4e986685ffdf2e3d1f6ab438
samvelie <sam.velie@gmail.com>
2020-06-21 20:21:15 -0500

    birthdate add, details screen update

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -14,6 +14,7 @@ namespace Vidly.Controllers
         public CustomersController()
         {
             _context = new ApplicationDbContext();
+
         }
 
         protected override void Dispose(bool disposing)
@@ -30,7 +31,7 @@ namespace Vidly.Controllers
 
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
