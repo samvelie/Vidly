bdd2020344ea3aa86026ea0bc9ef53ea262c098a
samvelie <sam.velie@gmail.com>
2020-06-25 20:38:59 -0500

    first basic validation

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,7 +10,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter a customer name.")]
         [StringLength(255)]
         public string Name { get; set; }
 
